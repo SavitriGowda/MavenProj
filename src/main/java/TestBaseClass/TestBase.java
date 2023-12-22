aca8f9ad4faece93d8cccb99bb089378e9033d7b
savitri <savitrgowda>
2023-12-22 17:14:03 +0530

    done changes in pom and testng TestBase class

--- src/main/java/TestBaseClass/TestBase.java
+++ src/main/java/TestBaseClass/TestBase.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -36,10 +37,13 @@ public class TestBase {
 	public static void initialize() {
 		String browserName=prop.getProperty("browser");
 		if(browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "C:\\Users"
-					+ "\\savit\\Downloads\\chromedriver-win64\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\savit\\Downloads\\geckodriver-v0.33.0-win64\\geckodriver.exe");
 			driver=new ChromeDriver();
 			
+		}else if(browserName.equalsIgnoreCase("firefox")){
+			System.setProperty("webdriver.gecko.driver", "C:\\Users"
+					+ "\\savit\\Downloads\\chromedriver-win64\\chromedriver.exe");
+			driver=new FirefoxDriver();
 		}
 		driver.get(prop.getProperty("url"));
 		
