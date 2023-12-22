16e5a26d6894fa7fba9f94547536669ac4a4196d
savitri <savitrgowda>
2023-12-22 14:25:42 +0530

    added report to homepage

--- src/main/java/TestBaseClass/TestBase.java
+++ src/main/java/TestBaseClass/TestBase.java
@@ -10,9 +10,14 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class TestBase {
 	public static Properties prop;
 	public static WebDriver driver;
+
 	
 	public TestBase() {
 		prop=new Properties();
@@ -37,7 +42,8 @@ public class TestBase {
 			
 		}
 		driver.get(prop.getProperty("url"));
+		
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 }
--- src/test/java/TestClass/HomePageTest.java
+++ src/test/java/TestClass/HomePageTest.java
@@ -5,6 +5,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 import Pages.HomePage;
 import Pages.ReservePage;
 import TestBaseClass.TestBase;
@@ -12,37 +16,54 @@ import TestBaseClass.TestBase;
 public class HomePageTest extends TestBase{
 	public HomePage homepage;
 	public ReservePage reservepage;
+	public static ExtentReports report;
+	public static ExtentTest test;
+	public static ExtentReports extent=new ExtentReports();
 	
+	ExtentSparkReporter spark=new ExtentSparkReporter("target/spark.html");
 	
 	public HomePageTest() {
 		super();
 	}
-	
+
 	@BeforeMethod
 	public void setUp() {
 		initialize();
+		extent.attachReporter(spark);
+		
 		homepage=new HomePage();
+	
+	
 	}
 	
 	@Test
 	public void homepageTitleTest() {
+		test=extent.createTest("Home Page title", "title of tha homa page test case");
+		test.pass("passed");
 		String title=homepage.HomePageTitle();
 		Assert.assertEquals(title, "Welcome to the Simple Travel Agency!");
+		
 	}
 	@Test
 	public void hyperlinkUrlTest() {
+		test=extent.createTest("Verify Link of vacation", "Link is working currect");
+		test.pass("passed");
 		String text=homepage.hyperLink();
 		Assert.assertTrue(text.contains("vacation"));
 		driver.navigate().back();
 	}
 	@Test
 	public void findTecketTest() {
+		test=extent.createTest("Verify Ticket booking button", "Verify Ticket booking button");
+		test.pass("passed");
 		reservepage=homepage.findTicket();
 		
 	}
 	
 	@AfterMethod
 	public void tearDown() {
+		
+		extent.flush();
 		driver.close();
 	}
 }
