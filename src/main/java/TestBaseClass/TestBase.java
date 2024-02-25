dedbb432a3e091bd27c031761f2a1ad5b9bd9694
savitri <savitrgowda>
2024-02-25 12:14:41 +0530

    first commit

--- src/main/java/TestBaseClass/TestBase.java
+++ src/main/java/TestBaseClass/TestBase.java
@@ -4,16 +4,14 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
+import java.util.Set;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class TestBase {
 	public static Properties prop;
@@ -49,5 +47,6 @@ public class TestBase {
 		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
 	}
 }
